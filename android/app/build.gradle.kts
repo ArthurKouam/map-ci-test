6a39dc0e68bd21549b2d0b443461307e6f71c2dd
ArthurKouam <gkouamarthur@gmail.com>
2026-05-05 20:25:44 +0100

    Fix: Carte

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.commando"
+    namespace = "com.jsdprojet.commando"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.commando"
+        applicationId = "com.jsdprojet.commando"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/jsdprojet/commando/MainActivity.kt
+++ android/app/src/main/kotlin/com/jsdprojet/commando/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.commando
+package com.jsdprojet.commando
 
 import io.flutter.embedding.android.FlutterActivity
 
